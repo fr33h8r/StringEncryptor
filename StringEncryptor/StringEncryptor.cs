d908522faa989f0921b3e00a4acd0ec327afc81f
Paul Zaloznyj <pzaloznyj@gmail.com>
2012-09-20 22:25:54 +0300

    first commit

---
+++ StringEncryptor/StringEncryptor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StringEncryptor
+{
+    public interface IEncryptor
+    {
+        string Convert(string s);
+    }
+
+    public class Encryptor : IEncryptor
+    {
+        private string CharToInt(string s)
+        {
+            return string.Join("_", s.ToCharArray().Select(a => (a == ' ' ? '\0' : (a - 'a' + 1))).ToList());
+        }
+
+        private string IntToChar(string s)
+        {
+            var result = new StringBuilder();
+            var list = s.Split('_').ToList();
+            list.ForEach(x => result.Append((char) (int.Parse(x) - 1 + 'a')));
+
+            return result.ToString();
+        }
+
+        public string Convert(string s)
+        {
+            return CharToInt(s);
+        }
+    }
+}
---
+++ StringEncryptor/Tests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace StringEncryptor
+{
+    public class EncryptorTests
+    {
+        [Fact]
+        public void should_get_char_return_number()
+        {
+            //act
+            var res = new Encryptor().Convert("a");
+            //assert
+            res.Should().BeEquivalentTo("1");
+        }
+
+        [Fact(Skip = "")]
+        public void should_get_int_return_char()
+        {
+            //act
+            //var res = Encryptor.IntToChar("1");
+            //assert
+            //res.Should().BeEquivalentTo("a");
+        }
+
+        [Fact]
+        public void should_take_string_of_chars_return_nums()
+        {
+            var res = new Encryptor().Convert("abcd");
+            res.Should().BeEquivalentTo("1_2_3_4");
+        }
+
+        [Fact(Skip = "")]
+        public void should_take_string_of_nums_return_chars()
+        {
+            //var res = Encryptor.IntToChar("1_2_3_4");
+            //res.Should().BeEquivalentTo("abcd");
+        }
+
+        [Fact]
+        public void should_take_chars_and_convert_them_to_nums()
+        {
+            //arrange
+            const string text = "ab cd";
+            const string expected = "1_2_0_3_4";
+            //act
+            var res = new Encryptor().Convert(text);
+            //assert
+            res.Should().BeEquivalentTo(expected);
+        }
+        [Fact(Skip = "")]
+        public void should_take_nums_and_convert_them_to_chars()
+        {
+            //arrange
+            const string text = "1_2_3_4";
+            const string expected = "abcd";
+            //act
+            var res = new Encryptor().Convert(text);
+            //assert
+            res.Should().BeEquivalentTo(expected);
+        }
+    }
+}
